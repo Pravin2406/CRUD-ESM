fdf13ad9923dcf16e9d75fc87c9321920f2c1a40
Pravin2406 <pravinkatariya53@gmail.com>
2023-11-29 18:09:55 +0530

    crud

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, require: true },
@@ -6,6 +7,10 @@ const userSchema = new mongoose.Schema({
   pass: { type: String, require: true },
 });
 
+userSchema.pre("save", async function () {
+  try {
+    this.pass = bcrypt.hash(this.pass, 12);
+  } catch (error) {}
+});
+
 export default new mongoose.model("User", userSchema);
-
-
